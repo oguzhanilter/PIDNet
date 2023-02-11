34e4d68988efa0e7377bf2dc6bf98d19c122dbf4
oguzhan <oguzhan@airetailersystems.com>
2023-02-12 00:18:28 +0100

    add run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-# source $HOME/begin.sh
+#!/bin/sh
 
-# $HOME/miniconda3/envs/ss/bin/python $HOME/PIDNet/tools/train.py --cfg $HOME/PIDNet/configs/kitti/pidnet_small_kitti_storage.yaml
-$HOME/miniconda3/envs/ss/bin/python $HOME/PIDNet/tools/custom.py --f dirs.txt
+$HOME/miniconda3/envs/ss/bin/python $HOME/PIDNet/tools/train.py --cfg $HOME/PIDNet/configs/kitti/pidnet_large_kitti_storage.yaml
+#$HOME/miniconda3/envs/ss/bin/python $HOME/PIDNet/tools/custom.py --f dirs.txt
