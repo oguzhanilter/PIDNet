be58f1f7beb620486deeb137951831445bd2b067
oguzhan <oguzhan@airetailersystems.com>
2023-02-20 21:05:14 +0100

    change runs

--- run4.sh
+++ run4.sh
@@ -1,4 +1,4 @@
 # source $HOME/begin.sh
 
 # $HOME/miniconda3/envs/ss/bin/python $HOME/PIDNet/tools/train.py --cfg $HOME/PIDNet/configs/kitti/pidnet_small_kitti_storage.yaml
-$HOME/miniconda3/envs/ss/bin/python $HOME/PIDNet/tools/custom.py --r /cluster/scratch/oilter/KITTI/sequences/04/image_0 --o 05
+$HOME/miniconda3/envs/ss/bin/python $HOME/PIDNet/tools/custom.py --r /cluster/scratch/oilter/KITTI/sequences/04/image_0 --o 04
